cbfc38203e42589887ebd6d1ff4d265eefb92a51
Jacob Zirbel <jacobzirbel@gmail.com>
2020-07-06 12:04:20 -0500

    prevent scraping more than once per day

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { scrapeOnion, scrapeReddit } = require("../scrape");
 const db = require("../models");
-router.route("/home").get((req, res) => {
-  res.render("index");
-});
+
 router.route("/play").get((req, res) => {
   db.Article.find({}).then((allArticles) => {
-    let articles = allArticles.sort(() => Math.random() - 0.5).slice(0, 7);
+    let articles = allArticles.sort(() => Math.random() - 0.5).slice(0, 2);
     articles = articles.map((e) => ({ headline: e.headline, id: e._id }));
     res.render("play", { articles });
   });
@@ -16,8 +14,8 @@ router.route("allarticles").get((req, res) => {
   // res.render all where correct+incorrect > 0
   // show link/headline
 });
-router.route("/scrape").get((req, res) => {
-  scrapeArticlesIntoDatabase((allArticles) => {
+router.route("/home").get((req, res) => {
+  scrapeArticlesIntoDatabase(() => {
     res.render("index");
   });
 });
@@ -25,21 +23,30 @@ router.route("/scrape").get((req, res) => {
 module.exports = router;
 
 function scrapeArticlesIntoDatabase(callback) {
-  // TODO: check when last scrape was, only scrape once per hour
-
-  // Run both scrapes and mongodb query concurrently
-  let dbPromise = db.Article.find({});
-  Promise.all([scrapeReddit(), scrapeOnion(), dbPromise]).then((allData) => {
-    let dbArticles = allData[2];
-    let scrapedArticles = [...allData[0], ...allData[1]];
-    // Remove from scrapedArticles if headline already exists in db
-    scrapedArticles = scrapedArticles.filter((scrapedArticle) => {
-      return !dbArticles.find((dbArticle) => {
-        return dbArticle.headline === scrapedArticle.headline;
+  const scrape = (articles) => {
+    Promise.all([scrapeReddit(), scrapeOnion()]).then((allData) => {
+      let dbArticles = articles;
+      let scrapedArticles = [...allData[0], ...allData[1]];
+      // Remove from scrapedArticles if headline already exists in db
+      scrapedArticles = scrapedArticles.filter((scrapedArticle) => {
+        return !dbArticles.find((dbArticle) => {
+          return dbArticle.headline === scrapedArticle.headline;
+        });
+      });
+      db.Article.insertMany(scrapedArticles).then(() => {
+        callback();
       });
     });
-    db.Article.insertMany(scrapedArticles).then(() => {
+  };
+  const DAY_IN_MS = 1000 * 60 * 60 * 24;
+  db.Article.find({}).then((articles) => {
+    let mostRecentScrape = articles
+      .map((a) => a.scrapedDate.getTime())
+      .sort((a, b) => b - a)[0];
+    if (mostRecentScrape < Date.now() - DAY_IN_MS) {
+      scrape(articles);
+    } else {
       callback();
-    });
+    }
   });
 }
